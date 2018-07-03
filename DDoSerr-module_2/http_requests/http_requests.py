d85c5eae9dfea9c7c8deb0930dd059fbcf658977
Konstantin-PS <konstantin.p.96@gmail.com>
2018-07-03 19:36:31 +0300

    Добавлен модуль для HTTP запросов. Немного вылизан модуль лога, но, видимо, я перестарался и слизал что-то важное. В общем, модуль лога сейчас не работает с 3 версией пайтона.

---
+++ DDoSerr-module_2/http_requests/http_requests.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+#Путь к интерпретатору пайтона (может работать и без этого, если в системе прописан).
+# -*- coding: utf-8 -*-
+#Выбор кодировки (без него не работает русский язык).
+
+"""
+Модуль отправки HTTP-запросов для DDoSerr.
+v.1.1.2 от 03.07.2018.
+"""
+
+#import httplib					#Подключаем модуль для работы с HTTP (низкоуровневый).
+
+#connection = httplib.HTTPConnection("https://httpbin.org/get")	#Будем соединяться с сайтом Microsoft.
+#connection.request("HEAD", "/index.html")					#Запрашиваем начальную страницу.
+#result = connection.getresponse()							#Записываем ответ.
+#print result.status, result.reason							#Выводим результат.
+
+
+#Способ лучше. С использованием надстройки над httplib для упрощения.
+import requests
+
+def http_connection():
+	request = requests.get('https://httpbin.org/get')
+	#request = requests.post("http://httpbin.org/post")
+	status = request.status_code
+	print(status)
+	head = request.headers['content-type']
+	print(head)
+
+http_connection()	#Выов функции.
