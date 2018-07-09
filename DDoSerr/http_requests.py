59953597d31f34e99e6f8bb3ff27f370e75fc9ea
Konstantin-PS <konstantin.p.96@gmail.com>
2018-07-09 15:45:00 +0300

    v.1.5.2b. от 09.07.2018. Есть логгирование, многопоточность. Пока ещё нет настроек из командной строки (и не все параметры доступны для настройки), алгоритма обхода сайта, юзер агентов. Немного вычищены файлы, считается размер ответа.

--- DDoSerr/http_requests.py
+++ DDoSerr/http_requests.py
@@ -5,7 +5,7 @@
 
 """
 Модуль отправки HTTP-запросов для DDoSerr.
-v.1.5.1b. от 09.07.2018.
+v.1.5.2b. от 09.07.2018.
 """
 
 """
@@ -26,10 +26,12 @@ import os
 import logging
 
 #Настройка логгирования.
-logging.basicConfig(filename='log.log',level=logging.INFO, format='%(asctime)s %(message)s', datefmt='%d.%m.%Y - %I:%M:%S |')
+logging.basicConfig(filename='log.log',level=logging.INFO, \
+format='%(asctime)s %(message)s', datefmt='%d.%m.%Y - %I:%M:%S |')
 #Пока не нужен подробный лог.
 #logging.basicConfig(filename='log.log',level=logging.DEBUG)
 
+#Перенос кода через '\'.
 
 def http_connection(repeat, pause, url="http://127.0.0.1"):
     """
@@ -44,41 +46,39 @@ def http_connection(repeat, pause, url="http://127.0.0.1"):
 
     
     for _ in range(repeat):
-        request = requests.get(url)
-        #Для post запросов пригодится в будущем.
+        request = requests.get(url, stream=True)
+        #Для post запросов, пригодится в будущем.
         #request = requests.post("http://httpbin.org/post")
     
         status = str(request.status_code)
-        head = str(request.headers['content-type'])
-        #Этот вывод результата можно убрать для повышения производительности.
-        print(status, head)
+        size = str(sum(len(chunk) for chunk in request.iter_content(8196)))
+        #Для типа.
+        #head = str(request.headers['content-type'])
         
-        answer = str('\t' + status + '\t' + head)
+        #Этот вывод результата можно убрать для повышения производительности.
+        print(status, size)
+        
+        answer = str("code " + status + "," + '\t' + "size " + size)
         
         #Логгируем запуск процесса с его PID.
         logging.info('Process ' + str(os.getpid()) + ' received the task.')
         #Логгируем результаты.
         logging.info('Answer: ' + answer)
         
-        #Для многопоточности. Вывод result в message.
-        #Из объекта берутся значения методом get() с таймаутом в 1 секунду.
-        ##message = str(result.get(timeout=1))
-        #message = str(status + '\t' + head)
-  
-        
         #Задержка перед повтором цикла.
         time.sleep(pause)
     
 """
-!!!Проверить одновременность работы через apply_async! 
+Проверить одновременность работы через apply_async! 
 Если не одновременно, то переделать под map_async!
+Работает.
 Надо через MAP так:
 https://stackoverflow.com/questions/11996632/multiprocessing-in-python-while-limiting-the-number-of-running-processes
 Перевод (кривенький): http://qaru.site/questions/306301/multiprocessing-in-python-while-limiting-the-number-of-running-processes
 
-
-
-Это должно работать, если вместо url сделать список адресов с задерками.
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+!Вместо url надо сделать список адресов с задерками.
+!~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
     
