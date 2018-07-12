94610ea105d609d49f11a5267e569b5b9577cacb
Konstantin-PS <konstantin.p.96@gmail.com>
2018-07-13 01:03:23 +0300

    v.1.5.4b. от 12.07.2018. Немного подчищены комментарии и исправлены мелкие ошибки. Модуль сортировки работает, но нуждается в оптимизации и подключении к модулю запросов.

--- DDoSerr/http_requests.py
+++ DDoSerr/http_requests.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
-#Путь к интерпретатору пайтона.
 # -*- coding: utf-8 -*-
-#Выбор кодировки (без него не работает русский язык).
 
 """
 Модуль отправки HTTP-запросов для DDoSerr.
-v.1.5.2b. от 09.07.2018.
+v.1.5.4b. от 12.07.2018.
 """
 
 """
@@ -28,32 +26,28 @@ import logging
 #Настройка логгирования.
 logging.basicConfig(filename='log.log',level=logging.INFO, \
 format='%(asctime)s %(message)s', datefmt='%d.%m.%Y - %I:%M:%S |')
-#Пока не нужен подробный лог.
-#logging.basicConfig(filename='log.log',level=logging.DEBUG)
 
-#Перенос кода через '\'.
 
 def http_connection(repeat, pause, url="http://127.0.0.1"):
     """
     Функция для создания одного запроса к сайту и получения ответа.
     """
-    #https://httpbin.org/get
     #Если не задаётся извне значение url, то берётся указанное.
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    #!Позже добавить в аргументы user_agent!
+    #!Добавить в аргументы user_agent!
     #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
     
 
     
     for _ in range(repeat):
-        request = requests.get(url, stream=True)
+        response = requests.get(url, stream=True)
         #Для post запросов, пригодится в будущем.
-        #request = requests.post("http://httpbin.org/post")
-    
-        status = str(request.status_code)
-        size = str(sum(len(chunk) for chunk in request.iter_content(8196)))
-        #Для типа.
-        #head = str(request.headers['content-type'])
+        #response = requests.post("http://httpbin.org/post")
+        
+        #Код возврата.
+        status = str(response.status_code)
+        #Размер ответа, большой ответ разбивается на куски по 8196 Байт.
+        size = str(sum(len(chunk) for chunk in response.iter_content(8196)))
         
         #Этот вывод результата можно убрать для повышения производительности.
         print(status, size)
@@ -69,46 +63,17 @@ def http_connection(repeat, pause, url="http://127.0.0.1"):
         time.sleep(pause)
     
 """
-Проверить одновременность работы через apply_async! 
-Если не одновременно, то переделать под map_async!
-Работает.
-Надо через MAP так:
-https://stackoverflow.com/questions/11996632/multiprocessing-in-python-while-limiting-the-number-of-running-processes
-Перевод (кривенький): http://qaru.site/questions/306301/multiprocessing-in-python-while-limiting-the-number-of-running-processes
-
 !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 !Вместо url надо сделать список адресов с задерками.
 !~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 """
 
-    
+
+if __name__ == "__main__":
 """
 То, что находится внутри if __name__ == "__main__" будет выполнено 
 только в том случае, когда модуль запущен отдельно, а не импортирован. 
 """
-if __name__ == "__main__":
-    """
-    Реализация многопоточности для самостоятельного запуска модуля.
-    Хорошо работает.
-    Но в докстринге, т.к. используется другой способ.
-    #Получается 4 процесса: один родительский и 3 дочерних.
-    
-    
-    #Создаём пул из 3-х работников (процессов).
-    pool = Pool(processes=3)
-    #Задаём набор параметров для задания (по количеству процессов + 1). 
-    #Пока что вручную.
-    urls = ["https://www.vsu.ru", "https://www.vsu.ru", "https://www.vsu.ru", "https://www.vsu.ru"]
-    #Даём им задание и получаем результат.
-    result = pool.map_async(http_connection, urls)
-    
-    #apply_async даёт задачу только одному процессу. Не подходит.
-    #result = pool.apply_async(http_connection, ("https://www.vsu.ru",))
-    
-    #Вывод результата с таймаутом (если вдруг будет работать слишком долго).
-    print(result.get(timeout=1))
-    """
-    
     #Запуск функции.
     http_connection(repeat, pause, url)
 
